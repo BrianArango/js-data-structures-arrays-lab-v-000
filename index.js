3dcd0c96bfd2ca7bc0efd1e7b33e617ef4a76ade
Brian Arango <brianvictus@gmail.com>
2019-06-07 00:19:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function destructivelyRemoveFirstDriver(name) {
 }
 
 function appendDriver(name) {
-  newDrivers = drivers.pop("Broom");
+  newDrivers = drivers.push("Broom");
   return newDrivers;
 }
